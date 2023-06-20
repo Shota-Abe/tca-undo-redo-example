594258164bdff2ccd8a1550f8c09fa7624f9fa34
Shota Abe <abesyouta516@gmail.com>
2023-06-20 23:41:40 +0900

    Initial Commit

---
+++ tca-undo-redo-example/tca_undo_redo_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  tca_undo_redo_exampleApp.swift
+//  tca-undo-redo-example
+//
+//  Created by 安部翔太 on 2023/06/20.
+//
+
+import SwiftUI
+
+@main
+struct tca_undo_redo_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
